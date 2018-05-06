81f8fd8f781c76e7f61f6b878b0eda451c5ce2b1
Ján Bušfy <busfy.jan@gmail.com>
2018-05-06 11:06:10 +0200

    dev env

--- src/module/firebaseInit.js
+++ src/module/firebaseInit.js
@@ -1,10 +1,21 @@
 import firebase from "firebase";
 
-firebase.initializeApp({
-    apiKey: "AIzaSyAxLUFulfX7PhX1NuaVQtiKcVgZsEH8TDc",
-    authDomain: "pray-for-me-123.firebaseapp.com",
-    databaseURL: "https://pray-for-me-123.firebaseio.com",
-    projectId: "pray-for-me-123",
-    storageBucket: "",
-    messagingSenderId: "377445716599"
-});
+firebase.initializeApp(
+    process.env.NODE_ENV === 'development'
+        ? {
+            apiKey: "AIzaSyDmt-i31VhqsNoh1KccAiuTpuwDbnD7chQ",
+            authDomain: "pray-for-me-dev.firebaseapp.com",
+            databaseURL: "https://pray-for-me-dev.firebaseio.com",
+            projectId: "pray-for-me-dev",
+            storageBucket: "pray-for-me-dev.appspot.com",
+            messagingSenderId: "486945506229"
+        }
+        : {
+            apiKey: "AIzaSyAxLUFulfX7PhX1NuaVQtiKcVgZsEH8TDc",
+            authDomain: "pray-for-me-123.firebaseapp.com",
+            databaseURL: "https://pray-for-me-123.firebaseio.com",
+            projectId: "pray-for-me-123",
+            storageBucket: "",
+            messagingSenderId: "377445716599"
+    }
+);
