2158cd60c8b16ca7720230a977f70424b7c1729e
Roman Busfy <roman.busfy@inloopx.com>
2018-07-24 10:51:47 +0200

    add new filter option

--- src/actions/prayer.js
+++ src/actions/prayer.js
@@ -1,30 +1,38 @@
-export const SET_LIST = "SET_LIST";
-export const SET_ONLY_MINE = "SET_ONLY_MINE";
-export const ADD_REFRESH_REQUEST = "ADD_REFRESH_REQUEST";
-export const POP_REFRESH_REQUEST = "POP_REFRESH_REQUEST";
+export const SET_LIST = 'SET_LIST'
+export const SET_ONLY_MINE = 'SET_ONLY_MINE'
+export const SET_ONLY_WITHOUT_DEEDS = 'SET_ONLY_WITHOUT_DEEDS'
+export const ADD_REFRESH_REQUEST = 'ADD_REFRESH_REQUEST'
+export const POP_REFRESH_REQUEST = 'POP_REFRESH_REQUEST'
 
 export const setOnlyMine = onlyMine => {
-    return {
-        type: SET_ONLY_MINE,
-        onlyMine: onlyMine
-    };
-};
+  return {
+    type: SET_ONLY_MINE,
+    onlyMine: onlyMine
+  }
+}
+
+export const setOnlyWithoutDeeds = withoutDeeds => {
+  return {
+    type: SET_ONLY_WITHOUT_DEEDS,
+    withoutDeeds: withoutDeeds
+  }
+}
 
 export const setList = list => {
-    return {
-        type: SET_LIST,
-        list: list
-    };
-};
+  return {
+    type: SET_LIST,
+    list: list
+  }
+}
 
 export const addRefreshRequest = () => {
-    return {
-        type: ADD_REFRESH_REQUEST
-    };
-};
+  return {
+    type: ADD_REFRESH_REQUEST
+  }
+}
 
 export const popRefreshRequest = () => {
-    return {
-        type: POP_REFRESH_REQUEST
-    };
-};
+  return {
+    type: POP_REFRESH_REQUEST
+  }
+}
---
+++ src/component/prayer/FilterWithoutDeedsButton.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import MaterialButton from 'material-ui/Button'
+import IconActive from 'material-ui-icons/VisibilityOff'
+import IconInactive from 'material-ui-icons/Visibility'
+import { blue } from 'material-ui/colors'
+
+const MaterialButtonStyled = styled(MaterialButton)`
+  && {
+    position: fixed !important;
+    right: 7px;
+    top: 100px;
+    z-index: 2;
+    background: ${blue[800]};
+
+    &:hover {
+      background: ${blue[700]};
+    }
+  }
+`
+
+class FilterWithoutDeedsButton extends Component {
+  render() {
+    const { onClick, active } = this.props
+
+    return (
+      <MaterialButtonStyled onClick={onClick} fab mini color="primary">
+        {active ? <IconActive /> : <IconInactive />}
+      </MaterialButtonStyled>
+    )
+  }
+}
+
+FilterWithoutDeedsButton.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  active: PropTypes.bool.isRequired
+}
+
+export default FilterWithoutDeedsButton
