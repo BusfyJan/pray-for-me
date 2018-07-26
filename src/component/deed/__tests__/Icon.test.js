4fb2bd3cb129f9c8c5698fab6d5a9fab0232d020
Roman Busfy <roman.busfy@inloopx.com>
2018-07-26 15:09:22 +0200

    delete Snaphots CI problems

--- src/component/deed/__tests__/Icon.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Icon from '../Icon'
-import renderer from 'react-test-renderer'
-
-test('Icon render', () => {
-  const component = renderer.create(<Icon type="mass" />)
-  let tree = component.toJSON()
-  expect(tree).toMatchSnapshot()
-})
--- src/component/loader/__tests__/FullCircularLoader.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import FullCircularLoader from '../FullCircularLoader'
-import renderer from 'react-test-renderer'
-
-test('FullCircularLoader render', () => {
-  const component = renderer.create(<FullCircularLoader />)
-  let tree = component.toJSON()
-  expect(tree).toMatchSnapshot()
-})
