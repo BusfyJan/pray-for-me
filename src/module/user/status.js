d1fd6f9ab0d527f7a4d2efe0fdf1930bc4045a62
Ján Bušfy <busfy.jan@gmail.com>
2018-08-31 20:18:17 +0200

    npm ci reverted + firebase

--- src/module/user/status.js
+++ src/module/user/status.js
@@ -1,4 +1,5 @@
-import firebase from 'firebase';
+import firebase from 'firebase/app';
+import 'firebase/database';
 import {getId as getUserId} from 'module/user/user.js';
 
 export const init = () => {
